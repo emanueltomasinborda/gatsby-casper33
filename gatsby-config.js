20d381ff5242a79772e541bb476b0ad527ed0e6f
Emanuel <7004075+emanueltomasinborda@users.noreply.github.com>
2020-02-07 08:36:59 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   siteMetadata: {
     title: 'Ghost',
-    description: 'The professional publishing platform',
-    siteUrl: 'https://gatsby-casper.netlify.com', // full path to blog - no ending slash
+    description: 'Noche',
+    siteUrl: 'https://noche.eu.org', // full path to blog - no ending slash
   },
   mapping: {
     'MarkdownRemark.frontmatter.author': 'AuthorYaml',
